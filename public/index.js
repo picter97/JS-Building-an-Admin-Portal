f9454719ec5e24e134651d29f31f3b68f12d772a
picter <picter2esolv@gmail.com>
2023-01-10 17:40:02 -0500

    last comit

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 async function main() {
 
-    let response = await fetch('http://localhost:3001/listBooks')
+    let response = await fetch('http://localhlistBooksost:3001/')
     let books = await response.json()
 
     books.forEach(renderBook)
